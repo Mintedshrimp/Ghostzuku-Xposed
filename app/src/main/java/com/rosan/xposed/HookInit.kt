259c42fc5ffdee4e558651b52a3e31fd79c17544
MintedShrimp <dupiesegan@gmail.com>
2026-04-14 10:39:09 +0200

    Update HookInit.kt

--- app/src/main/java/com/rosan/xposed/HookInit.kt
+++ app/src/main/java/com/rosan/xposed/HookInit.kt
@@ -6,7 +6,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 class HookInit : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         // Skip our own process
-        if (lpparam.packageName == "com.rosan.dhizuku.api.xposed") return
+        if (lpparam.packageName == "com.ghostzuku.xposed") return
 
         ShizukuHook(lpparam).start()
     }
